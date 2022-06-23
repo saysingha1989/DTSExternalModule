63efaedd15faa2e850cfdc78280ab349cf33dd4f
HP <sayantans432@gmail.com>
2022-06-23 21:41:52 +0530

    Initial Commit

---
+++ src/main/java/org/ford/DTSExternalModule/DtsExternalModuleApplication.java
@@ -0,0 +1,13 @@
+package org.ford.DTSExternalModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtsExternalModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtsExternalModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ford/DTSExternalModule/DtsExternalModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ford.DTSExternalModule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtsExternalModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
